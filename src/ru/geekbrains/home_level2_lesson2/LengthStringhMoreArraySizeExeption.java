518678b83f94a119c785daa57f13ffad4834d162
Andrey Kozlov <pilotc@bk.ru>
2020-04-09 04:35:33 +0600

    final

--- src/ru/geekbrains/home_level2_lesson2/LengthStringhMoreArraySizeExeption.java
+++ src/ru/geekbrains/home_level2_lesson2/LengthStringhMoreArraySizeExeption.java
@@ -1,6 +1,9 @@
 package ru.geekbrains.home_level2_lesson2;
 
-public class LengthStringhMoreArraySizeExeption extends ArrayIndexOutOfBoundsException {
+public class LengthStringhMoreArraySizeExeption extends RuntimeException {
 
+    public LengthStringhMoreArraySizeExeption (String s) {
+        super (s);
+    }
 
 }
